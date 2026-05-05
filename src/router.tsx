d0abcdecb7ac5cbfcf010f388f4c861c0987597a
Technology Universe <technologyuniverse364515@gmail.com>
2026-05-06 01:07:18 +0530

    required changes for github pages

--- src/router.tsx
+++ src/router.tsx
@@ -57,6 +57,7 @@ function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => vo
 export const getRouter = () => {
   const router = createRouter({
     routeTree,
+    basepath: "/brewhaus-cafe",
     context: {},
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
